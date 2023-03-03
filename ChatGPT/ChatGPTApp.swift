6fe13910f99f9dcd1a3d110f19df962bfffa4341
Adolfo <adolfo.vera@outlook.com>
2023-03-03 20:29:41 +0100

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Adolfo Vera Blasco on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
